bf4aa4340cc682af06c268f4a498111fdeb67b2a
Maurits Rijk <maurits.rijk@surf.nl>
2024-10-26 17:23:19 +0200

    Add formatters

---
+++ predicate/formatter/format_json.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from predicate import AlwaysFalsePredicate, AlwaysTruePredicate, AndPredicate
+from predicate.predicate import Predicate, OrPredicate, XorPredicate, NotPredicate
+
+
+def to_json(predicate: Predicate) -> dict[str, Any]:
+    def to_value(predicate) -> tuple[str, Any]:
+        match predicate:
+            case AlwaysFalsePredicate():
+                return "false", False
+            case AlwaysTruePredicate():
+                return "true", True
+            case AndPredicate(left, right):
+                return "and", {
+                    "left": to_json(left),
+                    "right": to_json(right)
+                }
+            case NotPredicate(predicate):
+                return "not", {"predicate": to_json(predicate)}
+            case OrPredicate(left, right):
+                return "or", {
+                    "left": to_json(left),
+                    "right": to_json(right)
+                }
+            case XorPredicate(left, right):
+                return "xor", {
+                    "left": to_json(left),
+                    "right": to_json(right)
+                }
+
+    return dict([to_value(predicate)])
---
+++ test/formatter/test_format_json.py
@@ -0,0 +1,74 @@
+from predicate import always_false_p, always_true_p
+from predicate.formatter.format_json import to_json
+
+
+def test_format_json_false():
+    predicate = always_false_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "false": False
+    }
+
+
+def test_format_json_true():
+    predicate = always_true_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "true": True
+    }
+
+
+def test_format_json_and():
+    predicate = always_true_p & always_false_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "and": {
+            "left": {"true": True},
+            "right": {"false": False}
+        }
+    }
+
+
+def test_format_json_or():
+    predicate = always_true_p | always_false_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "or": {
+            "left": {"true": True},
+            "right": {"false": False}
+        }
+    }
+
+
+def test_format_json_xor():
+    predicate = always_true_p ^ always_false_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "xor": {
+            "left": {"true": True},
+            "right": {"false": False},
+        }
+    }
+
+
+def test_format_json_not():
+    predicate = ~always_true_p
+
+    json = to_json(predicate)
+
+    assert json == {
+        "not": {
+            "predicate": {"true": True}
+        }
+    }
+
