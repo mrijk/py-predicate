37e76bc3ef5378a622957ccf3f45dc3b5330131b
Maurits Rijk <maurits.rijk@surf.nl>
2024-10-28 13:11:35 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,11 @@
 # -- Project information -----------------------------------------------------
 
-project = 'py-predicate'
-author = 'Maurits Rijk'
+project = "py-predicate"
+author = "Maurits Rijk"
 copyright = "2024, " + author
 
 # The full version, including alpha/beta/rc tags
-version = release = '0.1'
+version = release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 
@@ -13,11 +13,11 @@ version = release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages',
+    "sphinx.ext.githubpages",
     # 'sphinx_copybutton',
 ]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -25,12 +25,12 @@ pygments_style = 'sphinx'
 # The document name of the “master” document, that is,
 # the document that contains the root toctree directive.
 # Default is 'index', we set it here for supporting Sphinx<2.0
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # A boolean that decides whether codeauthor and sectionauthor directives
 # produce any output in the built files.
