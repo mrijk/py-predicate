61b5fb14458e967a3bba6195967a9b3820dff084
Maurits Rijk <maurits.rijk@surf.nl>
2024-11-02 16:35:36 +0100

    Add documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,37 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "py-predicate"
-author = "Maurits Rijk"
-copyright = "2024, " + author
-
-# The full version, including alpha/beta/rc tags
-version = release = "0.1"
+project = 'py-predicate'
+copyright = '2024, Maurits Rijk'
+author = 'Maurits Rijk'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.githubpages",
-    # 'sphinx_copybutton',
-]
+extensions = []
 
-pygments_style = "sphinx"
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
 
-# The document name of the “master” document, that is,
-# the document that contains the root toctree directive.
-# Default is 'index', we set it here for supporting Sphinx<2.0
-master_doc = "index"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# A boolean that decides whether codeauthor and sectionauthor directives
-# produce any output in the built files.
-show_authors = True
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,17 @@
-Introduction
-============
+.. py-predicate documentation master file, created by
+   sphinx-quickstart on Sat Nov  2 16:34:23 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-This library makes it easy to define composable predicates.
+py-predicate documentation
+==========================
 
-.. code-block:: python
-    def foo() -> bool:
-        return False
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
