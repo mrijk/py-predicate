3b7578a6197361231310863f57159b0947eb9163
Maurits Rijk <maurits.rijk@surf.nl>
2024-11-26 19:39:27 +0100

    Add copy button

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions: list = ["sphinx.ext.graphviz"]
+extensions: list = ["sphinx.ext.graphviz", "sphinx_copybutton"]
 
 graphviz_output_format = "svg"
 
