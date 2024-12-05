e207a3ea8c249a6d6071bdc2ad4fb9681b492265
Maurits Rijk <maurits.rijk@surf.nl>
2024-12-05 22:21:17 +0100

    Rename ip

--- test/test_ip_address_predicates.py
+++ test/test_ip_address_predicates.py
@@ -1,49 +1,49 @@
 from ipaddress import IPv4Network
 
 from predicate.ip_address_predicates import (
-    is_ipv4_global_p,
-    is_ipv4_link_local_p,
-    is_ipv4_loopback_p,
-    is_ipv4_multicast_p,
-    is_ipv4_private_p,
-    is_ipv4_reserved_p,
-    is_ipv4_unspecified_p,
+    is_ipv4_network_global_p,
+    is_ipv4_network_link_local_p,
+    is_ipv4_network_loopback_p,
+    is_ipv4_network_multicast_p,
+    is_ipv4_network_private_p,
+    is_ipv4_network_reserved_p,
+    is_ipv4_network_unspecified_p,
     subnet_of_p,
     supernet_of_p,
 )
 
 
 def test_ipv4_network_is_global():
-    assert not is_ipv4_global_p(IPv4Network("100.64.0.0/10"))
-    assert not is_ipv4_global_p(IPv4Network("127.0.0.0/29"))
+    assert not is_ipv4_network_global_p(IPv4Network("100.64.0.0/10"))
+    assert not is_ipv4_network_global_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_multicast():
-    assert not is_ipv4_multicast_p(IPv4Network("127.0.0.0/29"))
-    assert is_ipv4_multicast_p(IPv4Network("224.0.0.0/4"))
+    assert not is_ipv4_network_multicast_p(IPv4Network("127.0.0.0/29"))
+    assert is_ipv4_network_multicast_p(IPv4Network("224.0.0.0/4"))
 
 
 def test_ipv4_network_is_private():
-    assert not is_ipv4_private_p(IPv4Network("100.64.0.0/10"))
-    assert is_ipv4_private_p(IPv4Network("127.0.0.0/29"))
+    assert not is_ipv4_network_private_p(IPv4Network("100.64.0.0/10"))
+    assert is_ipv4_network_private_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_loopback():
-    assert is_ipv4_loopback_p(IPv4Network("127.0.0.0/29"))
+    assert is_ipv4_network_loopback_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_reserved():
-    assert is_ipv4_reserved_p(IPv4Network("240.0.0.0/4"))
-    assert not is_ipv4_reserved_p(IPv4Network("127.0.0.0/29"))
+    assert is_ipv4_network_reserved_p(IPv4Network("240.0.0.0/4"))
+    assert not is_ipv4_network_reserved_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_link_local():
-    assert is_ipv4_link_local_p(IPv4Network("169.254.0.0/16"))
-    assert not is_ipv4_link_local_p(IPv4Network("127.0.0.0/29"))
+    assert is_ipv4_network_link_local_p(IPv4Network("169.254.0.0/16"))
+    assert not is_ipv4_network_link_local_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_unspecified():
-    assert not is_ipv4_unspecified_p(IPv4Network("127.0.0.0/29"))
+    assert not is_ipv4_network_unspecified_p(IPv4Network("127.0.0.0/29"))
 
 
 def test_ipv4_network_is_subnet_of():
