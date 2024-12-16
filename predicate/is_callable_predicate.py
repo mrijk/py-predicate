a27616d9ea93068b977d42a103613294396d63fe
Maurits <maurits.rijk@gmail.com>
2024-12-16 22:34:59 +0100

    63 check signature (#65)
    * Fix build
    
    * Fix build
    
    * Add new predicates
    
    * Bump version
    
    ---------
    
    Co-authored-by: Maurits Rijk <maurits.rijk@surf.nl>

---
+++ predicate/is_callable_predicate.py
@@ -0,0 +1,41 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Any, override
+
+from predicate.predicate import Predicate
+
+
+@dataclass
+class IsCallablePredicate[T](Predicate[T]):
+    """A predicate class that models the is_callable predicate."""
+
+    params: list
+    return_type: Any
+
+    def __call__(self, x: Any) -> bool:
+        match x:
+            case Callable() as c:  # type: ignore
+                annotations = c.__annotations__
+                return_type = annotations["return"]
+                params = [param for key, param in annotations.items() if key != "return"]
+
+                return params == self.params and return_type == self.return_type
+            case _:
+                return False
+
+    def __repr__(self) -> str:
+        return "is_callable_p"
+
+    @override
+    def explain_failure(self, x: Any) -> dict:
+        match x:
+            case Callable() as c:  # type: ignore
+                annotations = c.__annotations__
+                return_type = annotations["return"]
+
+                if return_type != self.return_type:
+                    return {"result": False, "reason": f"Wrong return type: {return_type}"}
+
+                return {"result": False, "reason": "tbd"}
+            case _:
+                return {"result": False, "reason": f"{x} is not a Callable"}
