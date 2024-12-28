9d8576dd121a1e2478b4c963d6df9d2def0c7fe5
Maurits Rijk <maurits.rijk@surf.nl>
2024-12-28 22:46:25 +0100

    Improve predicate generator

--- predicate/generator/helpers.py
+++ predicate/generator/helpers.py
@@ -70,7 +70,7 @@ def random_datetimes(lower: datetime | None = None, upper: datetime | None = Non
 def random_predicates(*, max_depth: int = 10) -> Iterator:
     subclasses = Predicate.__subclasses__()
 
-    iterables = [generate_predicate(subclass, max_depth=max_depth) for subclass in subclasses]
+    iterables = [generate_predicate(subclass, max_depth=max_depth) for subclass in subclasses]  # type: ignore
 
     yield from random_first_from_iterables(*iterables)
 
