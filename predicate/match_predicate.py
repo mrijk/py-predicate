adb6992bce7376eab732765c3ebf79088b713cfb
Maurits Rijk <maurits.rijk@surf.nl>
2025-02-02 18:40:25 +0100

    Add match_p

--- predicate/match_predicate.py
+++ predicate/match_predicate.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Callable, Iterable, override
 
+from predicate import always_true_p
 from predicate.predicate import Predicate
 
 
@@ -8,7 +9,7 @@ from predicate.predicate import Predicate
 class MatchPredicate[T](Predicate[T]):
     """A predicate class that models 'match iterable' predicate."""
 
-    predicates: list[Predicate]
+    predicates: list[Callable]
 
     def __call__(self, iterable: Iterable[T]) -> bool:
         return match(self.predicates, iterable)
@@ -21,15 +22,17 @@ class MatchPredicate[T](Predicate[T]):
         return {"reason": "Tbd"}
 
 
-def match_p[T](*predicates: Predicate) -> MatchPredicate[T]:
+def match_p[T](*predicates: Callable) -> MatchPredicate[T]:
     """Return True if the predicate holds for each item in the iterable, otherwise False."""
     return MatchPredicate(predicates=list(predicates))
 
 
-def match(predicates: list[Predicate], iterable: Iterable) -> bool:
+def match(predicates: list[Callable], iterable: Iterable) -> bool:
     predicate, *rest_predicates = predicates
     match predicate:
         case Predicate():
+            if not iterable:
+                return False
             item, *rest = iterable
             if rest_predicates:
                 return predicate(item) and match(rest_predicates, rest)
@@ -54,7 +57,7 @@ def repeat(m: int, n: int, predicate: Predicate) -> Callable:
 def exactly_n(n: int, predicate: Predicate) -> Callable:
     """Match exactly n instances of the given predicate."""
 
-    def _exactly_n(predicates: list[Predicate], iterable: Iterable) -> bool:
+    def _exactly_n(predicates: list[Callable], iterable: Iterable) -> bool:
         rest = iterable
         for _ in range(n):
             if not rest:
@@ -68,13 +71,34 @@ def exactly_n(n: int, predicate: Predicate) -> Callable:
     return _exactly_n
 
 
+def star(predicate: Predicate) -> Callable:
+    """Match any instances of the given predicate."""
+
+    def _star(predicates: list[Callable], iterable: Iterable) -> bool:
+        if not iterable:
+            return not predicates
+        item, *rest = iterable
+        if predicate(item):
+            if _star(predicates, rest):
+                return True
+            if predicates:
+                matched = match(predicates, rest)
+                return match(predicates, iterable) if not matched else True  # backtrack
+        return False
+
+    return _star
+
+
 def optional(predicate: Predicate) -> Callable:
     """Match 0 or 1 instances of the given predicate."""
 
-    def _optional(predicates: list[Predicate], iterable: Iterable) -> bool:
+    def _optional(predicates: list[Callable], iterable: Iterable) -> bool:
         if not iterable:
             return True
         item, *rest = iterable
         return (predicate(item) and match(predicates, rest)) or match(predicates, iterable)
 
     return _optional
+
+
+wildcard = star(always_true_p)
--- test/test_match_predicate.py
+++ test/test_match_predicate.py
@@ -1,5 +1,5 @@
-from predicate import eq_p, is_int_p, is_str_p
-from predicate.match_predicate import exactly_n, match_p, optional, repeat
+from predicate import eq_p, is_float_p, is_int_p, is_str_p
+from predicate.match_predicate import exactly_n, match_p, optional, repeat, star, wildcard
 
 
 def test_match_first():
@@ -59,4 +59,69 @@ def test_repeat():
 
     predicate = match_p(one_to_three)
 
+    assert not predicate([])
     assert predicate([1])
+    assert predicate([1, 2])
+    assert predicate([1, 2, 3])
+
+
+def test_repeat_and_exactly():
+    one_to_three = repeat(1, 3, is_int_p)
+
+    predicate = match_p(one_to_three, is_int_p)
+
+    assert not predicate([])
+    assert not predicate([1])
+    assert predicate([1, 2])
+
+
+def test_repeat_and_exactly_and_repeat():
+    one_to_three_ints = repeat(1, 3, is_int_p)
+    two_to_three_strings = repeat(2, 3, is_str_p)
+
+    predicate = match_p(one_to_three_ints, is_float_p, two_to_three_strings)
+
+    assert not predicate([1, 2, 3.0, "foo"])
+    assert predicate([1, 2, 3.0, "foo", "bar"])
+
+
+def test_star():
+    zero_or_more_ints = star(is_int_p)
+
+    predicate = match_p(zero_or_more_ints)
+
+    assert not predicate(["foo"])
+    assert predicate([])
+    assert predicate([1, 2])
+
+
+def test_star_and_str():
+    zero_or_more_ints = star(is_int_p)
+
+    predicate = match_p(zero_or_more_ints, is_str_p)
+
+    assert predicate([1, 2, "foo"])
+
+
+def test_any_int():
+    predicate = match_p(wildcard, is_int_p, wildcard)
+
+    assert not predicate(["foo", "bar"])
+    assert predicate(["foo", "bar", 1, "foo", "bar"])
+
+
+def test_int_followed_by_str():
+    predicate = match_p(wildcard, is_int_p, is_str_p, wildcard)
+
+    assert not predicate(["foo", 1])
+    assert predicate([3.14, 1, "foo", 3.14])
+
+
+def test_exactly_with_not():
+    two_strings = exactly_n(2, is_str_p)
+
+    predicate = match_p(two_strings, optional(~is_str_p))
+
+    # TODO
+    # assert not predicate(["foo", "bar", "foobar"])
+    assert predicate(["foo", "bar"])
