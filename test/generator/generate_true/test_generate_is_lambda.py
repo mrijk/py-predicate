2557a8fa2fe1c19082b1a67085c7c203f3d72cb0
Maurits Rijk <maurits.rijk@surf.nl>
2025-06-12 15:10:20 +0200

    Update

--- test/generator/generate_true/test_generate_is_lambda.py
+++ test/generator/generate_true/test_generate_is_lambda.py
@@ -1,7 +1,12 @@
 import pytest
 
 from generator.generate_true.helpers import assert_generated_true
-from predicate import is_lambda_with_signature_p
+from predicate import is_lambda_p, is_lambda_with_signature_p
+
+
+def test_generate_is_lambda():
+    predicate = is_lambda_p
+    assert_generated_true(predicate)
 
 
 @pytest.mark.parametrize("nr_of_parameters", list(range(0, 5)))
